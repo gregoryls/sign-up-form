d0f9095297106d017c82151a044772b7a2772325
Greg Schneider <greg53x@msn.com>
2022-05-23 00:04:53 -0600

    clearing password match error specifically selects error span

--- js.js
+++ js.js
@@ -1,15 +1,18 @@
 
 function onChange() {
     const confirmDiv = document.querySelector('.confirm');
-    const span = document.createElement('span')
+    const span = document.createElement('span');
+    const spanSelector = document.querySelector('.password-error');
     const password = document.querySelector('input[name=userPassword]');
     const confirm = document.querySelector('input[name=confirmPassword]');
     if (confirm.value === password.value) {
       confirm.setCustomValidity('');
-      confirmDiv.removeChild(confirmDiv.lastChild);
+    //   confirmDiv.removeChild(confirmDiv.lastChild);
+      spanSelector.remove();
     } else {
       confirm.setCustomValidity('Passwords do not match');
       confirmDiv.appendChild(span);
+      span.classList.add('password-error');
       span.textContent = 'Passwords do not match';
     }
   }
