e7bcdd6878881066ae122d1985a70ec0de3518a7
Greg Schneider <greg53x@msn.com>
2022-05-22 23:56:04 -0600

    password error message clears when passwords match

--- js.js
+++ js.js
@@ -1,10 +1,15 @@
 
 function onChange() {
+    const confirmDiv = document.querySelector('.confirm');
+    const span = document.createElement('span')
     const password = document.querySelector('input[name=userPassword]');
     const confirm = document.querySelector('input[name=confirmPassword]');
     if (confirm.value === password.value) {
       confirm.setCustomValidity('');
+      confirmDiv.removeChild(confirmDiv.lastChild);
     } else {
       confirm.setCustomValidity('Passwords do not match');
+      confirmDiv.appendChild(span);
+      span.textContent = 'Passwords do not match';
     }
   }
